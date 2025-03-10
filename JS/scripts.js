f842d0664b2e8c78a8a687592a9ea6a6ad5f3d69
Prykhodkina <in_extremis@ymail.com>
2025-03-10 15:59:31 +0200

    Dz13

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,18 +1,67 @@
-$('.owl-carousel').owlCarousel({
-    autoplay:true,
-    autoplayTimeout: 6500,
-    autoplaySpeed: 1000,
-    loop: true,
-    responsive:{
-        0:{
-            items:1
-        },
-        1100:{
-            items:2
-        }
-    }
+$(document).ready(function(){
 
-})
+    $('.owl-carousel').owlCarousel({
+        autoplay:true,
+        autoplayTimeout: 6500,
+        autoplaySpeed: 1000,
+        loop: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            1100:{
+                items:2
+            }
+        }
+    })
+
+});
+
 $( function() {
     $( "#accordion" ).accordion();
 } );
+(function($) { "use strict";
+
+
+    //Navigation
+
+    var app = function () {
+        var body = undefined;
+        var menu = undefined;
+        var menuItems = undefined;
+        var init = function init() {
+            body = document.querySelector('body');
+            menu = document.querySelector('.menu-icon');
+            menuItems = document.querySelectorAll('.nav__list-item');
+            applyListeners();
+        };
+        var applyListeners = function applyListeners() {
+            menu.addEventListener('click', function () {
+                return toggleClass(body, 'nav-active');
+            });
+        };
+        var toggleClass = function toggleClass(element, stringClass) {
+            if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+        };
+        init();
+    }();
+
+
+    //Switch light/dark
+
+    $("#switch").on('click', function () {
+        if ($("body").hasClass("light")) {
+            $("body").removeClass("light");
+            $("#switch").removeClass("switched");
+        }
+        else {
+            $("body").addClass("light");
+            $("#switch").addClass("switched");
+        }
+    });
+
+})(jQuery);
+
+
+
+
